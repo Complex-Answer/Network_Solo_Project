bdb435ae049e21a57d87df1a104d532d4ba2e1a9
Complex-Answer <zstep0101@gmail.com>
2026-01-23 18:21:25 +0900

    Feat:맵 실험 중
    맵 실험중입니다
    아직 완성된게 없습니다

--- Assets/1. Script/Player/CTRL/PlayerMove.cs
+++ Assets/1. Script/Player/CTRL/PlayerMove.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerMove : MonoBehaviour
 {
@@ -19,11 +20,27 @@ public class PlayerMove : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (_player.PlayerDash.IsDash)
+        if (!_player.PlayerDash.IsDash)
         {
-            return;
+            _player.Rb.linearVelocity = new Vector3(_move.x * _player.MoveSpeed, 0, _move.y * _player.MoveSpeed);
         }
-        _player.Rb.linearVelocity = new Vector3(_move.x * _player.MoveSpeed, 0,_move.y * _player.MoveSpeed);
+
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(_player.Rb.position, out hit, 1.2f, NavMesh.AllAreas))
+        {
+            float distance = Vector3.Distance(_player.Rb.position, hit.position);
+
+            if (distance > 0.1f)
+            {
+                _player.Rb.position = hit.position; // 위치를 즉시 안으로 고정
+
+                // 대시 중이라면 속도를 0으로 만들어 뚫고 나가는 힘을 제거
+                if (_player.PlayerDash.IsDash)
+                {
+                    _player.Rb.linearVelocity = Vector3.zero;
+                }
+            }
+        }
     }
     
 }
