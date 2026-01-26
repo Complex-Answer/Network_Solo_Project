b2122f550009bba74425ba251c0658cf7890ca93
Complex-Answer <zstep0101@gmail.com>
2026-01-26 10:46:39 +0900

    Feat: 플레이어 마우스 트래킹
    플레이어에게 마우스를 바라보게하는 기능을 추가했습니다

--- Assets/1. Script/Player/CTRL/TrackingMouse.cs
+++ Assets/1. Script/Player/CTRL/TrackingMouse.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TrackingMouse : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float rotationSpeed = 10f;
+    [SerializeField] private LayerMask groundLayer;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        RotationToMouse();
+    }
+
+    private void RotationToMouse()
+    {
+        Vector2 mouseScreenPos = Mouse.current.position.ReadValue();
+        Ray ray = Camera.main.ScreenPointToRay(mouseScreenPos);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit,Mathf.Infinity, groundLayer))
+        {
+            Vector3 targetPosition = hit.point;
+            targetPosition.y = transform.position.y;
+
+            Vector3 dir = (targetPosition - transform.position).normalized;
+
+            if (dir != Vector3.zero)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(dir);
+                transform.rotation  = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed*Time.deltaTime);
+                 Debug.DrawRay(transform.position, dir * 5f, Color.red);
+            }
+        }
     }
 }
