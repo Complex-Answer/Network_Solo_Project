6da325a11f01216c10cb7af3b974a0bea6658d6a
Complex-Answer <zstep0101@gmail.com>
2026-01-30 12:11:12 +0900

    Feat: 포탈 및 몬스터 스폰 관련 설정
    몬스터 배열을 만들고 그 중에서 랜덤으로 뽑아서 소환시켜줍니다
    포탈과 상호작용을 완료시켰습니다

--- Assets/1. Script/Camera/FollowPlayer.cs
+++ Assets/1. Script/Camera/FollowPlayer.cs
@@ -2,21 +2,26 @@ using Photon.Pun;
 using Unity.Cinemachine;
 using UnityEngine;
 
-public class FollowPlayer : MonoBehaviour
+public class FollowPlayer : MonoBehaviourPun
 {
     void Start()
     {
-        PhotonView pv = GetComponent<PhotonView>();
-
-        if (pv.IsMine)
+        if (photonView.IsMine)
         {
-            //CinemachineCamera cinema = 
+            var vcam = FindFirstObjectByType<CinemachineCamera>();
+
+            if (vcam != null)
+            {
+                vcam.Follow = transform;
+                vcam.LookAt = transform;
+
+                Debug.Log($"{gameObject.name}: 카메라 연결 완료!");
+            }
+            else
+            {
+                Debug.LogWarning("씬에 CinemachineCamera가 없습니다!");
+            }
         }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
